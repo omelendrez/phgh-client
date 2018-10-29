24a9765d096e44cd0fe296273905bdb5ae8ec25b
omelendrez <omar.melendrez@gmail.com>
2018-10-29 02:05:23 -0300

    Add validation and random user name generator to signup form

--- src/components/forms/LoginForm.vue
+++ src/components/forms/LoginForm.vue
@@ -3,8 +3,12 @@
     <v-container>
       <v-card-text>
         <v-form>
-          <v-text-field prepend-icon="person" name="username" label="Username" type="text"></v-text-field>
-          <v-text-field prepend-icon="lock" name="password" label="Password" id="password" type="password"></v-text-field>
+          <v-text-field prepend-icon="person" v-model="username" label="Username" hint="At least 8 characters" type="text" :rules="[rules.required, rules.min]">
+          </v-text-field>
+
+          <v-text-field prepend-icon="lock" v-model="password" label="Password" hint="At least 8 characters" :append-icon="showPassword ? 'visibility_off' : 'visibility'" :type="showPassword ? 'text' : 'password'" :rules="[rules.required, rules.min]" @click:append="showPassword = !showPassword">
+          </v-text-field>
+
         </v-form>
       </v-card-text>
       <v-card-actions>
@@ -17,8 +21,23 @@
     </v-container>
   </v-card>
 </template>
+
 <script>
+import { rules } from '@/utils/validation'
+
 export default {
-  name: 'LoginForm'
+  name: 'LoginForm',
+  data() {
+    return {
+      valid: false,
+      username: null,
+      password: null,
+      showPassword: false,
+      rules: null
+    }
+  },
+  created() {
+    this.rules = rules
+  }
 }
 </script>
