417c9254c521f6dbcb455e2adc3b5b559453c3d2
omelendrez <omar.melendrez@gmail.com>
2018-11-04 03:00:10 -0300

    Allow 11 digits for nigerian phone numbers

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -2,7 +2,7 @@ const rules = {
   required: v => !!v || 'Required',
   min: v => v.length >= 6 || 'Min 6 characters',
   isEmail: v => /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v) || 'E-mail must be valid',
-  isPhone: v => /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(v) || 'Phone number is not valid',
+  isPhone: v => /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{5})$/.test(v) || 'Phone number is not valid',
   equal: (v, y) => v === y || 'Passwords don\'t match'
 }
 
